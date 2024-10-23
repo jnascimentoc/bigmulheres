fbd675f2ad889fd6907df7b34613c75e5bf51188
jnascimentoc <jnascimentonc@gmail.com>
2024-10-23 18:17:22 -0300

    Versão corrigida do exercício

--- UC2/aula01_22102024_atividade.sql
+++ UC2/aula01_22102024_atividade.sql
@@ -1,12 +1,13 @@
 -- ATIVIDADE 
 --Crie uma nova tabela chamada 'professores', com a mesma quantidade de características de 'alunos', fazendo ao menos duas injeções de dados e uma consulta.
 
+-- create a table
 CREATE TABLE professores (
-    id INT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    genero VARCHAR(1) NOT NULL,   
+    id INTEGER PRIMARY KEY,
+    nome TEXT NOT NULL,
+    genero TEXT NOT NULL
 );
-
+-- insert some values
 INSERT INTO professores VALUES (1,'Ana Maria', 'F');
 INSERT INTO professores VALUES (2,'Carlos Henrique', 'M');
 INSERT INTO professores VALUES (3,'Julia do Nascimento', 'F');
@@ -16,4 +17,5 @@ INSERT INTO professores VALUES (6,'Viviane Almeida', 'F');
 INSERT INTO professores VALUES (7,'Clarissa Castro', 'F');
 INSERT INTO professores VALUES (8,'Fabio Paulo', 'M');
 
-SELECT * FROM professores WHERE genero = 'F'
+-- fetch some values
+SELECT * FROM professores WHERE genero = 'M'
